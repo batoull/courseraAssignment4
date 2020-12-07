0b74ba9235c69612534dcab4a6e7f63b47814793
batoull <batoull@users.noreply.github.com>
2020-12-07 17:09:28 +0800

    change the name

--- speakGoodBye.js
+++ speakGoodBye.js
@@ -21,9 +21,9 @@ var speakWord = "Good Bye";
 // See Lecture 52, part 2
 
 
-byeSpeaker.speak = function (name) {
+byeSpeaker.speak = function (names) {
 	 
-  console.log(speakWord + " " +name);
+  console.log(speakWord + " " +names);
 }
 
   window.byeSpeaker =  byeSpeaker;
